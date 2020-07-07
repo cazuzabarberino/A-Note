3e6a2268816e034b69f1e594552f024557492dc5
cazuzaneves <cazuzaneves@gmail.com>
2020-07-07 14:59:45 -0300

    Updates

---
+++ precache-manifest.664183bb7fb122d47ab95acc2e83e749.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "540f087cd4e02e203e2059ee9b6f2b4a",
+    "url": "/A-Note/index.html"
+  },
+  {
+    "revision": "4fae0a876ef398067871",
+    "url": "/A-Note/static/js/2.073c76e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/A-Note/static/js/2.073c76e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b7d9b2618d4f6d3849",
+    "url": "/A-Note/static/js/main.fa6e6953.chunk.js"
+  },
+  {
+    "revision": "9a7f842209ad90f5e693",
+    "url": "/A-Note/static/js/runtime-main.c621b5e8.js"
+  }
+]);
