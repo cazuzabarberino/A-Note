bef9bfa440af2ac1e6a4e7a1b11318b2580fa0d1
cazuzaneves <cazuzaneves@gmail.com>
2020-07-07 16:44:17 -0300

    globalStyle

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
 }
 
 export default App;
